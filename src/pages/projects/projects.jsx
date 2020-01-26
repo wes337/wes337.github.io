f4671a5691542e567e22715365bc15d08b442155
Wesley Moses <wesmoses@gmail.com>
2020-01-26 20:04:04 +0200

    build

--- src/pages/projects/projects.jsx
+++
@@ -1,8 +0,0 @@
-import aStarImg from '../../assets/images/screenshots/astar.png'
-
-export const aStar = ({
-    key: 'aStar',
-    title: 'A* Search Algorithm',
-    info: 'JavaScript (React, ES6)',
-    image: aStarImg,
-})
